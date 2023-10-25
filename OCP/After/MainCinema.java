02c5d91fefab4f5e727be1c95734e176f1411cdd
dilaa920 <abdilalstr20@gmail.com>
2023-10-25 23:23:39 +0700

    Menambah source code sebelum dan sesudah penerapan OCP

---
+++ OCP/After/MainCinema.java
@@ -0,0 +1,7 @@
+class MainCinema {
+	 public static void main(String[] args) {
+		 StandardCinema standardCinema = new StandardCinema(100.0);
+		 Double adminFee = standardCinema.calculateAdminFee();
+ 		System.out.println(adminFee);
+ 	}
+}
